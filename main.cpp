a76b11eca8b0e687a6ce5cc974e720e8b744a714
a.savanovich <asavanster@gmail.com>
2020-04-03 20:26:00 +0300

    add executable

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
-    QTranslator qtTranslator;
-    qtTranslator.load("qt-tray_" + QLocale::system().name());
-    app.installTranslator(&qtTranslator);
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     engine.load(url);
